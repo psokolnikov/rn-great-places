fcd376cce9c59c944d7fdb637430efec50cde3c5
psokolnikov <sokolnikov90@gmail.com>
2021-12-07 15:26:51 +0300

    Store Places in SQLite

---
+++ store/places-reducer.js
@@ -0,0 +1,26 @@
+import Place from '../models/place';
+import { ADD_PLACE, SET_PLACES } from './places-actions';
+
+const initialState = {
+	places: [],
+};
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case ADD_PLACE:
+			const newPlace = new Place(
+				action.placeData.id.toString(),
+				action.placeData.title,
+				action.placeData.image
+			);
+			return {
+				places: state.places.concat(newPlace),
+			};
+		default:
+			return state;
+		case SET_PLACES:
+			return {
+				places: action.places.map(pl => new Place(pl.id.toString(), pl.title, pl.imageUri))
+			};
+	}
+};
